7510c333cc05a16bd34ab562aae024d34672a619
abdiasiis78 <gureyxalane78@gmail.com>
2023-09-30 17:28:26 +0300

    fixing

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'src/main.js'),
+        main: path.resolve(__dirname, 'src/index.js'),
         '**/*.jsx': path.resolve(__dirname, 'src/**/*.jsx'),
       },
       // ...other rollup options
