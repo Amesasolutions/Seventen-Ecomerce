86eb40a40933b70bab24a1b59a683a62b1d4c9ec
abdiasiis78 <gureyxalane78@gmail.com>
2023-09-30 17:37:36 +0300

    error-fixing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {store} from './redux/store.jsx'
-import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
-)
+  </Provider>,
+  document.getElementById('root')
+);
