86a2cf3dc1c86f2bc6c2f5e32bf8db01e78edc7d
abdiasiis78 <gureyxalane78@gmail.com>
2023-09-30 17:55:09 +0300

    err-fx

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path'; // Import the 'path' module
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  // Your custom Vite configuration
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'src/main.jsx'),
-        '**/*.jsx': path.resolve(__dirname, 'src/**/*.jsx'),
-      },
-      // ...other rollup options
-    },
-  },
-});
+})
